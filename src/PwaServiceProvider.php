dadf1e133c7d59852d1c17430aac6cd6f836f470
Dmi3yy <dmi3yy@gmail.com>
2020-04-01 18:58:55 +0300

    fix artisan command

--- src/PwaServiceProvider.php
+++ src/PwaServiceProvider.php
@@ -6,8 +6,19 @@ use EvolutionCMS\ServiceProvider;
 
 class PwaServiceProvider extends ServiceProvider
 {
+    /**
+     * custom commands
+     * @var array
+     */
+    protected $commands = [
+        'EvolutionCMS\Dmi3yy\Pwa\Console\PwaCommand'
+    ];
+
+
     public function register()
     {
+        //register command for Artisan
+        $this->commands($this->commands);
 //        $this->loadPluginsFrom(
 //            dirname(__DIR__) . '/plugins/'
 //        );
