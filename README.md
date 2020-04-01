e24e6ad635c32c8bcbf8baf2b740c535f1412488
Dmi3yy <dmi3yy@gmail.com>
2020-04-01 13:16:48 +0300

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Evolution CMS 2.0 PWA
+Evolution CMS 2.0 Progressive Web App package
+
+## Install
+
+`php artisan package:installrequire dmi3yy/evocms-pwa '*'` in you **core/** folder
+
+## Установка  
+
+`php artisan package:installrequire dmi3yy/evocms-pwa '*'` в папке **core/**
---
+++ src/PwaServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace EvolutionCMS\Dmi3yy\Pwa;
+
+use EvolutionCMS\ServiceProvider;
+
+class PwaServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+//        $this->loadPluginsFrom(
+//            dirname(__DIR__) . '/plugins/'
+//        );
+        echo 'test-pwa';
+    }
+}
